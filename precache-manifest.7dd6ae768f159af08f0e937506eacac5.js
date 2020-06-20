d933d8189ce3766d2a00355a058062119202cf1f
jaga <jagadheesh6@gmail.com>
2020-06-20 09:08:12 +0530

    Updates

---
+++ precache-manifest.7dd6ae768f159af08f0e937506eacac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ee2362f0498756f9b5ad9c4aecf78a",
+    "url": "/invoice-generator/index.html"
+  },
+  {
+    "revision": "d7b866e1ded42ab12ec4",
+    "url": "/invoice-generator/static/css/main.45a9165b.chunk.css"
+  },
+  {
+    "revision": "7885de573741b9c91d9e",
+    "url": "/invoice-generator/static/js/2.37c56b09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invoice-generator/static/js/2.37c56b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b866e1ded42ab12ec4",
+    "url": "/invoice-generator/static/js/main.7f8d1f56.chunk.js"
+  },
+  {
+    "revision": "a5bfff1729eb505b942a",
+    "url": "/invoice-generator/static/js/runtime-main.fadf07e5.js"
+  }
+]);
