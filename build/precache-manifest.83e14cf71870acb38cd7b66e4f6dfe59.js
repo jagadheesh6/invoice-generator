388a7a68440160bfca46c282d0a614c4421ec544
Jaga <58385390+jagadheesh6@users.noreply.github.com>
2020-06-20 12:14:18 +0530

    Add files via upload

---
+++ build/precache-manifest.83e14cf71870acb38cd7b66e4f6dfe59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6823bae6a7d7c64a293d7e28a44aaccd",
+    "url": "/invoice-generator/index.html"
+  },
+  {
+    "revision": "c88b79a6e12dfabd7b2d",
+    "url": "/invoice-generator/static/css/main.733efd3b.chunk.css"
+  },
+  {
+    "revision": "2b78319a64e54f853c14",
+    "url": "/invoice-generator/static/js/2.f746fb07.chunk.js"
+  },
+  {
+    "revision": "6de07e5aec012398ec95763a9e7531f9",
+    "url": "/invoice-generator/static/js/2.f746fb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88b79a6e12dfabd7b2d",
+    "url": "/invoice-generator/static/js/main.64631bb9.chunk.js"
+  },
+  {
+    "revision": "a5bfff1729eb505b942a",
+    "url": "/invoice-generator/static/js/runtime-main.fadf07e5.js"
+  }
+]);
